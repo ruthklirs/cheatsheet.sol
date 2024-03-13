d8fca4641d61217076e4de2a56b3346d380c75df
ruthklirs <116270058+ruthklirs@users.noreply.github.com>
2024-03-13 16:56:48 +0200

    Update 1_Storage.sol

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -6,23 +6,19 @@ pragma solidity >=0.7.0 <0.9.0;
  * @title Storage
  * @dev Store & retrieve value in a variable
  */
-contract Storage {
-
+pragma solidity >=0.7.0 <0.9.0;
+//class that stores variables
+contract Storge{
+    //number type variable with 250 bits
     uint256 number;
-
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
-    function store(uint256 num) public {
-        number = num;
+    //function that initializes the number variable
+    function store (uint256 num) public {
+        number=num;
     }
-
-    /**
-     * @dev Return value
-     * @return value of 'number'
-     */
-    function retrieve() public view returns (uint256){
+    //view: This indicates that the function does not modify the contract's state.
+  //non-editable public function that returns the number
+    function retrieve() public  view returns (uint256){
         return number;
     }
+    
 }
